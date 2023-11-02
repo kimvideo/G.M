d5d7b16ca6e094cf4264042fe006cbb4eb78fbe8
gangvia <tail6622@naver.com>
2023-11-02 12:48:23 +0900

    Revert "주석 추가 + 오타 수정 + 일부 Activity간 연결"
    This reverts commit 15f6d6bf9c305a127116c49de8a121262a3bf839.

--- app/src/main/java/com/example/capstone_myapplication/Login_MainActivity.kt
+++ app/src/main/java/com/example/capstone_myapplication/Login_MainActivity.kt
@@ -1,19 +1,11 @@
 package com.example.capstone_myapplication
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class Login_MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_main)
-
-        val Join = Intent(this, Join_MainActivity::class.java)
-        val joinButton = findViewById<Button>(R.id.joinBtn)
-        joinButton.setOnClickListener {
-            startActivity(Join)
-        }
     }
 }
